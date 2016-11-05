6740ed044c9dabc751f45d60b25926b47310382c
Ethan Trinh <etonxt@gmail.com>
2016-11-05 06:26:50 -0400

    added webpages creategroup etc

--- app/views.py
+++ app/views.py
@@ -2,9 +2,11 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 from .forms import SignUpForm
+from .forms import GroupForm
 from werkzeug.wsgi import DispatcherMiddleware
 from web.api import app as api
 from flask import Flask, render_template
+
 
 # index view function suppressed for brevity
 @app.route('/index')
@@ -40,3 +42,31 @@ def login():
 @app.route('/landing')
 def landing():
     return render_template('landing.html')
+
+@app.route('/')
+def home():
+    groups = ["Naked & Afraid", "Frat stars", "bitches", "schmoes","Rachel"]
+    username = "Kevin Kozlowski"
+    return render_template('home.html',
+                            groups=groups,
+                            username=username)
+
+@app.route('/group')
+def group():
+    members = [{'name': "bob jones", 'money': 70},
+              {'name': "malcolm", 'money': 80},
+              {'name': "diana", 'money': -3},
+              {'name': "stanley payne",'money': 666}]
+    groupname = "Twelve"
+    return render_template('group.html',
+                            members=members,
+                            groupname=groupname)
+
+@app.route('/creategroup', methods=['GET', 'POST'])
+def creategroup():
+    form = GroupForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    return render_template('creategroup.html', 
+                           title='Create Group',
+                           form=form)
